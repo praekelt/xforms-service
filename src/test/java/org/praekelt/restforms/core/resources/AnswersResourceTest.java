bf74720b86ee716214e9c193790dd6ce88248dff
Ant Cosentino <antony@mycosentino.com>
2014-09-29 14:46:22 +0200

    added newline and removed exception checking helper.

--- src/test/java/org/praekelt/restforms/core/resources/AnswersResourceTest.java
+++ src/test/java/org/praekelt/restforms/core/resources/AnswersResourceTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 import org.praekelt.restforms.core.services.jedis.JedisClient;
 import org.praekelt.restforms.core.services.jedis.JedisFactory;
 import org.praekelt.restforms.core.services.rosa.RosaFactory;
@@ -20,7 +18,6 @@ public class AnswersResourceTest {
     
     private static JedisClient jedisClient;
     private AnswersResource instance;
-    private AnswersResource exceptionInstance;
     private String instanceId;
     private String exceptionInstanceId;
     private static final String form = "<h:html xmlns=\"http://www.w3.org/2002/xforms\" xmlns:h=\"http://www.w3.org/1999/xhtml\" xmlns:ev=\"http://www.w3.org/2001/xml-events\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:jr=\"http://openrosa.org/javarosa\"><h:head><h:title>xforms service form</h:title><model><instance><person><name></name><surname></surname><gender></gender><blah></blah></person></instance><bind nodeset=\"name\" type=\"string\" /><bind nodeset=\"surname\" type=\"string\" /><bind nodeset=\"gender\" type=\"string\" /><bind nodeset=\"blah\" type=\"int\" /></model></h:head><h:body><input ref=\"name\"><label>what's your name?</label></input><input ref=\"surname\"><label>what's your surname?</label></input><input ref=\"gender\"><label>what's your gender?</label></input><input ref=\"blah\"><label>what's your blah?</label></input></h:body></h:html>";
@@ -43,7 +40,6 @@ public class AnswersResourceTest {
     @Before
     public void setUp() throws Exception {
         instance = new AnswersResource(jedisClient);
-        exceptionInstance = new AnswersResource(jedisClient);
         
         if (instance == null) {
             fail("Couldnt initialise AnswersResource for testing.");
@@ -51,18 +47,9 @@ public class AnswersResourceTest {
         
         String completedForm;
         instanceId = instance.createResource("form", form);
-        exceptionInstanceId = exceptionInstance.createResource("form", form);
         
         if (instanceId == null || instanceId.isEmpty()) {
             fail("Couldn't add xForm template to Redis.");
-        }
-        
-        if (exceptionInstanceId == null || exceptionInstanceId.isEmpty()) {
-            fail("Couldn't add xForm template to Redis.");
-        }
-        
-        if (!exceptionInstance.updateResource(exceptionInstanceId, "lkjadsflkjasf".getBytes())) {
-            fail("Couldn't add fake serialised RosaFactory to Redis");
         }
         
         RosaFactory rf = new RosaFactory();
@@ -95,9 +82,6 @@ public class AnswersResourceTest {
             fail("Couldn't remove all keys from Redis storage.");
         }
     }
-    
-    @Rule
-    public ExpectedException e = ExpectedException.none();
     
     /**
      * Test of getSingle method, of class AnswersResource.
